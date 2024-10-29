b72738f2adbc28750dc48a55eaaee6594d9420f9
Christopher Charbonneau <ccharbooneau@gmail.com>
2024-10-28 22:50:15 -0400

    Prepare for creating demos

---
+++ demo/3D/Demo3D.cs
@@ -0,0 +1,15 @@
+using Godot;
+
+namespace Operations;
+
+public partial class Demo3D : Node3D
+{
+
+	public override void _Ready()
+	{
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
